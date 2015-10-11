490e0249654fbae205e567e64d242647c61b287b
John Sholar <johnmsholar@gmail.com>
2015-10-10 21:55:04 -0700

    First implementation of Chill Requests.

--- netflix.py
+++ netflix.py
@@ -1,5 +1,6 @@
 import os
 import json
+import datetime
 
 from flask import Flask, request, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -24,17 +25,6 @@ class User(db.Model):
     self.netflix_username = username
     self.netflix_password = password
 
-class ChillRequest(db.Model):
-  __tablename__ = 'chill_requests'
-  id = db.Column(db.Integer(), nullable=False, primary_key=True, unique=True, autoincrement=True)
-  #id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-  genre = db.Column(db.String(20))
-  film = db.Column(db.Boolean())
-  date = db.Column(db.Date())
-  time_of_day = db.Column(db.String(20))
-  latitude = db.Column(db.Float)
-
 def add_user(nf_un, nf_pw):
   new_user = User(nf_un, nf_pw)
   db.session.add(new_user)
@@ -50,6 +40,34 @@ def get_user_by_username(nf_un):
 
 def get_user_by_id(nf_id):
   return User.query.filter_by(user_id=nf_id).all()
+
+class ChillRequest(db.Model):
+  __tablename__ = 'chill_requests'
+  id = db.Column(db.Integer(), nullable=False, primary_key=True, unique=True, autoincrement=True)
+  #id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  genre = db.Column(db.String(20))
+  program_type = db.Column(db.Boolean())
+  date = db.Column(db.Date())
+  time_of_day = db.Column(db.String(20))
+  latitude = db.Column(db.Float())
+  longitude = db.Column(db.Float())
+
+  def __init__(self, user_id, genre, program_type, date, time_of_day, latitude, longitude):
+    self.user_id = user_id
+    self.genre = genre
+    self.program_type = program_type
+    self.date = date
+    self.time_of_day = time_of_day
+    self.latitude = latitude
+    self.longitude = longitude
+
+def add_chill_request(user_id, genre, program_type, date, time_of_day, latitude, longitude):
+  new_request = ChillRequest(user_id, genre, program_type, date, time_of_day, latitude, longitude)
+  db.session.add(new_request)
+  db.session.flush()
+  db.session.commit()
+  print 'Chill Request added successfully.'
 
 def verify_netflix_credentials(nf_un, nf_pw):
   """
@@ -108,6 +126,34 @@ def sign_in():
   # Case 3: User doesn't exist, and has invalid credentials
   else:
     return create_user_id_response(INVALID_NETFLIX_CREDENTIALS)
+
+@app.route('/create-chill-request', methods=['POST'])
+def create_chill_request:
+  request_data = json.loads(request.data)
+  user_id = request_date['uid']
+  genre = request_data['genre']
+  program_type = request_data['type']
+  date = process_date_from_string(request_data['day'])
+  time = request_data['time']
+  latitude = float(request_data['latitude'])
+  longitude = float(request_data['longitude'])
+  add_chill_request(user_id, genre, program_type, date, time, latitude, longitude)
+
+# Takes in a string denoting a day of the week, and returns a date object representing
+# the next occurence of that date
+def process_date_from_string(date_string):
+  today = date.today()
+  current_weekday = today.weekday()
+  target_weekday = get_weekday_int_from_string(date_string)
+  delta = datetime.timedelta(days=target_weekday-current_weekday)
+  return today + delta
+
+# Returns a Python standard integer representing day of the week. Monday is 0. Sunday is 6.
+def get_weekday_int_from_string(s):
+  days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  if s not in days:
+    raise ValueError('Day of week does not match any predefined names.')
+  return days.index(s)
 
 def create_user_id_response(user_id):
   return jsonify(**{'user_id': user_id})
